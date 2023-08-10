66da8f41516a862bf8d12e2c05b622b60329fc23
Chris Dunn <cdunn@stu.ncu.edu.jm>
2023-08-10 15:48:58 -0400

    Update for Set Price

---
+++ src/views/ProfileView.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <BackAppBar />
+        <div class="pa-5">
+            <h1>Profile</h1>
+            <div>
+                <div>User Name: {{ userStore.user?.userName }}</div>
+                <div>User Email: {{ userStore.user?.userEmail }}</div>
+                <div>Level: {{ userStore.user?.type }}</div>
+                <div class="pt-5">
+                    <v-btn color="primary" class="mr-5" @click="GeneratePin()">Generate Pin</v-btn>
+                    <v-btn color="primary" @click="ResetPin()">Reset Pin Lock</v-btn>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import BackAppBar from '@/components/BackAppBar.vue';
+import { useUserStore } from '@/stores/UserStore.js'
+
+const userStore = useUserStore();
+
+function ResetPin(){
+    userStore.ResetPin()
+}
+</script>
